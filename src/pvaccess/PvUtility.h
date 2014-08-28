63b94e9cb0bd3c6490430264c039a56d198a27f1
Sinisa Veseli <sveseli@anl.gov>
2014-08-28 15:09:25 -0500

    fixes for compiling latest changes agains 4.3.0

--- src/pvaccess/PvUtility.h
+++ src/pvaccess/PvUtility.h
@@ -6,11 +6,23 @@
 namespace PvUtility 
 {
 
+size_t fromString(const epics::pvData::PVStructurePtr& pvStructure, const epics::pvData::StringArray& from, size_t fromStartIndex = 0);
+
+#if defined PVA_API_VERSION && PVA_API_VERSION == 430
+
+void fromString(const epics::pvData::PVScalarPtr& pvScalar, const epics::pvData::String& from);
+
+size_t fromStringArray(const epics::pvData::PVScalarArrayPtr& pv, size_t offset, size_t length, const epics::pvData::StringArray& from, size_t fromOffset);
+
+size_t convertFromStringArray(epics::pvData::PVScalarArray* pv, size_t offset, size_t len, const epics::pvData::StringArray& from, size_t fromOffset);
+
+#else
+
 size_t fromString(const epics::pvData::PVScalarArrayPtr& pv, const epics::pvData::StringArray& from, size_t fromStartIndex = 0);
 
 size_t fromString(const epics::pvData::PVStructureArrayPtr& pv, const epics::pvData::StringArray& from, size_t fromStartIndex = 0);
 
-size_t fromString(const epics::pvData::PVStructurePtr& pvStructure, const epics::pvData::StringArray& from, size_t fromStartIndex = 0);
+#endif // if defined PVA_API_VERSION && PVA_API_VERSION == 430
 
 }
 
