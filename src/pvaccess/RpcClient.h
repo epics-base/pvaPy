5d5af2c3c32c1fea465f8ce6fd4a91e49a1ce942
Dave Hickin <david.hickin@diamond.ac.uk>
2016-07-27 19:49:24 +0100

    Add ability to specify pvRequest in RPC client
    Modify autoconf to check for 4.6 API. Ensure backwards compatibility
    with previous versions. Make clearer the distinction between pvRequest
    and arguments of the RPC.

--- src/pvaccess/RpcClient.h
+++ src/pvaccess/RpcClient.h
@@ -20,13 +20,16 @@ public:
     static const int DefaultTimeout;
 
     RpcClient(const std::string& channelName);
+#if defined PVA_RPC_API_VERSION && PVA_RPC_API_VERSION == 460
+    RpcClient(const std::string& channelName, const PvObject& pvRequestObject);
+#endif
     RpcClient(const RpcClient& pvaRpcClient);
     std::string getChannelName() const;
 
     virtual ~RpcClient();
     virtual epics::pvData::PVStructurePtr request(const epics::pvData::PVStructurePtr& pvRequest, double timeout=DefaultTimeout);
     //virtual PvObject* request(const PvObject& pvObject, double timeout=DefaultTimeout);
-    virtual PvObject* invoke(const PvObject& pvObject);
+    virtual PvObject* invoke(const PvObject& pvArgumentObject);
 
 private:
     static epics::pvAccess::RPCClient::shared_pointer createRpcClient(const std::string& channelName, const epics::pvData::PVStructurePtr& pvRequest, double timeout=DefaultTimeout);
@@ -35,6 +38,7 @@ private:
     bool rpcClientInitialized;
     epics::pvAccess::RPCClient::shared_pointer rpcClient;
     std::string channelName;
+    epics::pvData::PVStructure::shared_pointer pvRequest;
 };
 
 inline std::string RpcClient::getChannelName() const
