64b0e564f7cdff92e795b709b9f55c7321b3d648
Sinisa Veseli <sveseli@aps.anl.gov>
2017-05-25 11:06:03 -0500

    add test for pvaClient

---
+++ src/pvaccess/testPvaClient.cpp
@@ -0,0 +1,96 @@
+// Copyright information and license terms for this software can be
+// found in the file LICENSE that is included with the distribution
+
+#include <sys/time.h>
+#include <iostream>
+#include <unistd.h>
+
+#include "pv/pvaClient.h"
+
+class ChannelStateRequesterImpl : public epics::pvaClient::PvaClientChannelStateChangeRequester
+{
+public:
+    POINTER_DEFINITIONS(ChannelStateRequesterImpl);
+
+    ChannelStateRequesterImpl(bool& connected_) : connected(connected_) {}
+    virtual ~ChannelStateRequesterImpl() {}
+
+    bool isConnected() const {return connected;}
+
+    // PvaClientChannelStateChangeRequester interface
+    virtual void channelStateChange(const epics::pvaClient::PvaClientChannelPtr& channel, bool isConnected) {connected=isConnected;}
+
+private:
+    bool& connected;
+};
+
+class ChannelMonitorRequesterImpl : public epics::pvaClient::PvaClientMonitorRequester
+{
+public:
+    POINTER_DEFINITIONS(ChannelMonitorRequesterImpl);
+    ChannelMonitorRequesterImpl(const std::string& channelName_) :
+        channelName(channelName_),
+        isActive(true) {}
+    ChannelMonitorRequesterImpl(const ChannelMonitorRequesterImpl& channelMonitor) :
+        channelName(channelMonitor.channelName),
+        isActive(true) {}
+
+    virtual ~ChannelMonitorRequesterImpl() {}
+    virtual void event(const epics::pvaClient::PvaClientMonitorPtr& monitor)
+    {
+        if (nReceived == 0) {
+            gettimeofday(&startTime, NULL);
+        }
+        try {
+            while (isActive) {
+                if (!monitor->poll()) {;
+                    break;
+                }
+                nReceived++;
+                //printf("Got data %u\n", nReceived);
+                if (nReceived % 100000 == 0) {
+                    struct timeval currentTime;
+                    gettimeofday(&currentTime, NULL);
+                    double deltaT = (currentTime.tv_sec - startTime.tv_sec) + (currentTime.tv_usec - startTime.tv_usec)/1000000.0;
+                    double receiveRate = nReceived/deltaT/1000.0;
+                    printf("%s: Received: %u (%.2f [kHz])\n", channelName.c_str(), nReceived, receiveRate);
+                }
+                monitor->releaseEvent();
+            }
+        }
+        catch (std::runtime_error& ex) {
+            std::cerr << ex.what() << std::endl;
+        }
+    }
+
+
+    virtual void unlisten() {isActive=false;}
+
+private:
+    std::string channelName;
+    bool isActive;
+    unsigned long nReceived;
+    struct timeval startTime;
+};
+
+
+int main(int argc, char** argv)
+{
+    epics::pvaClient::PvaClientPtr pvaClientPtr(epics::pvaClient::PvaClient::create());
+
+    std::string channelName = "X1";
+    epics::pvaClient::PvaClientChannelPtr pvaClientChannelPtr(pvaClientPtr->createChannel(channelName, "pva"));
+    bool isConnected = false;
+    epics::pvaClient::PvaClientChannelStateChangeRequesterPtr stateRequester(epics::pvaClient::PvaClientChannelStateChangeRequesterPtr(new ChannelStateRequesterImpl(isConnected)));
+    pvaClientChannelPtr->setStateChangeRequester(stateRequester);
+
+    epics::pvaClient::PvaClientMonitorRequesterPtr monitorRequester(new ChannelMonitorRequesterImpl(channelName));
+    epics::pvaClient::PvaClientMonitorPtr monitor = pvaClientChannelPtr->monitor("field(value)", monitorRequester);
+    std::cout << "STARTED MONITOR for " << channelName << std::endl;
+    sleep(60);
+    monitor->stop();
+    std::cout << "STOPPED MONITOR for " << channelName << std::endl;
+    sleep(1);
+}
+
+
