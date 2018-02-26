a847d13edac2328de6b8abaf8de82b602367d7a6
Sinisa Veseli <sveseli@aps.anl.gov>
2018-02-26 11:26:14 -0600

    added soft links to executables in bin/EPICS_HOST_ARCH for pvapy-epics package

--- tools/conda/pvapy-epics-conda/build.sh
+++ tools/conda/pvapy-epics-conda/build.sh
@@ -3,12 +3,18 @@
 # Builds epics for conda
 
 echo "Building epics"
+EPICS_HOST_ARCH=`./startup/EpicsHostArch`
 eval "cat configure/CONFIG_SITE | sed 's?#INSTALL_LOCATION=.*?INSTALL_LOCATION=$PREFIX?' > configure/CONFIG_SITE.2 && mv configure/CONFIG_SITE.2 configure/CONFIG_SITE" 
 make -j
 make install 
 
 echo "Preparing epics installation"
 rsync -arvlP startup $PREFIX/
+cd $PREFIX/bin
+for f in `ls -c1 ./$EPICS_HOST_ARCH/*`; do 
+    ln -s $f .
+done
+
 
 
 
