ab65345a2e3222e2b974bde44f101dc367a3b3bb
Sinisa Veseli <sveseli@aps.anl.gov>
2018-07-06 20:16:03 -0500

    do not copy executables for now

--- tools/pip/epics-base-pip/setup.py
+++ tools/pip/epics-base-pip/setup.py
@@ -3,6 +3,7 @@ from distutils.command.build_ext import build_ext
 import os
 import platform
 import fnmatch
+##import tokenize
 
 DEFAULT_EPICS_BASE_VERSION = '7.0.1.1'
 MODULE_NAME = 'epics-base'
@@ -11,6 +12,19 @@ PLATFORM = platform.uname()[0].lower()
 BUILD_SCRIPT = './build.%s.sh' % PLATFORM
 DEPLOY_CONF = os.environ.get('DEPLOY_CONF', 'non_existent_file')
 EXECUTABLES=['caget', 'cainfo', 'camonitor', 'caput', 'caRepeater', 'eget', 'pvget', 'pvinfo', 'pvlist', 'pvput', 'softIoc', 'softIocPVA']
+
+### Workaround for python3 scripts issue with binary files 
+##try:
+##    _detect_encoding = tokenize.detect_encoding
+##except AttributeError:
+##    pass
+##else:
+##    def detect_encoding(readline):
+##        try:
+##            return _detect_encoding(readline)
+##        except SyntaxError:
+##            return 'utf-8', []
+##tokenize.detect_encoding = detect_encoding
 
 def get_env_var(name, default):
     value = os.environ.get(name)
@@ -40,7 +54,7 @@ class BuildExt(build_ext):
 
 MODULE_VERSION = get_env_var('EPICS_BASE_VERSION', DEFAULT_EPICS_BASE_VERSION) 
 MODULE_FILES = map(lambda f: f.replace('%s/' % MODULE_NAME, ''), find_files(MODULE_NAME))
-MODULE_SCRIPTS = [f for f in find_files('%s/bin' % MODULE_NAME) if os.path.basename(f) in EXECUTABLES]
+##MODULE_SCRIPTS = [f for f in find_files('%s/bin' % MODULE_NAME) if os.path.basename(f) in EXECUTABLES]
 
 setup(
   name = MODULE_NAME,
@@ -52,7 +66,7 @@ setup(
   package_data = {
     MODULE_NAME : MODULE_FILES,
   },
-  scripts = MODULE_SCRIPTS,
+  ##scripts = MODULE_SCRIPTS,
   ext_modules=[MODULE],
   cmdclass = {'build_ext': BuildExt}
 )
