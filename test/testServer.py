aade41df8d19d2bb3d7361a0972b08fb24c93f29
Sinisa Veseli <sveseli@aps.anl.gov>
2019-03-28 16:00:09 -0500

    fix argument type

---
+++ test/testServer.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import time
+import sys
+
+import pvaccess as pva
+
+class TestServer(pva.PvaServer):
+
+    BOOLEAN_CHANNEL_NAME = 'testBoolean'
+    BYTE_CHANNEL_NAME = 'testByte'
+    UBYTE_CHANNEL_NAME = 'testUByte'
+    SHORT_CHANNEL_NAME = 'testShort'
+    USHORT_CHANNEL_NAME = 'testUShort'
+    INT_CHANNEL_NAME = 'testInt'
+    UINT_CHANNEL_NAME = 'testUInt'
+    LONG_CHANNEL_NAME = 'testLong'
+    ULONG_CHANNEL_NAME = 'testULong'
+    FLOAT_CHANNEL_NAME = 'testFloat'
+    DOUBLE_CHANNEL_NAME = 'testDouble'
+    STRING_CHANNEL_NAME = 'testString'
+
+    def __init__(self):
+        pva.PvaServer.__init__(self)
+        self.booleanPv = pva.PvBoolean()
+        self.addRecord(self.BOOLEAN_CHANNEL_NAME, self.booleanPv)
+
+        self.bytePv = pva.PvByte()
+        self.addRecord(self.BYTE_CHANNEL_NAME, self.bytePv)
+
+        self.ubytePv = pva.PvUByte()
+        self.addRecord(self.UBYTE_CHANNEL_NAME, self.ubytePv)
+
+        self.shortPv = pva.PvShort()
+        self.addRecord(self.SHORT_CHANNEL_NAME, self.shortPv)
+
+        self.ushortPv = pva.PvUShort()
+        self.addRecord(self.USHORT_CHANNEL_NAME, self.ushortPv)
+
+        self.intPv = pva.PvInt()
+        self.addRecord(self.INT_CHANNEL_NAME, self.intPv)
+
+        self.uintPv = pva.PvUInt()
+        self.addRecord(self.UINT_CHANNEL_NAME, self.uintPv)
+
+        self.longPv = pva.PvLong()
+        self.addRecord(self.LONG_CHANNEL_NAME, self.longPv)
+
+        self.ulongPv = pva.PvULong()
+        self.addRecord(self.ULONG_CHANNEL_NAME, self.ulongPv)
+
+        self.floatPv = pva.PvFloat()
+        self.addRecord(self.FLOAT_CHANNEL_NAME, self.floatPv)
+
+        self.doublePv = pva.PvDouble()
+        self.addRecord(self.DOUBLE_CHANNEL_NAME, self.doublePv)
+
+        self.stringPv = pva.PvString('')
+        self.addRecord(self.STRING_CHANNEL_NAME, self.stringPv)
+
+    def __del__(self):
+        records = self.getRecordNames()
+        for r in records:
+            self.removeRecord(r)
+
+######################################################################
+# run test server
+if __name__ == '__main__':
+    SERVER_RUN_TIME = 30
+    if len(sys.argv) > 1:
+        SERVER_RUN_TIME = float(sys.argv[1])
+    server = TestServer()
+    time.sleep(SERVER_RUN_TIME)
+
+
+
