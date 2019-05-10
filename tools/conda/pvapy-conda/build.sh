20ee67362f5587cbc849650bd125c1c653ff8001
Sinisa Veseli <sveseli@aps.anl.gov>
2019-05-10 14:09:38 -0500

    add automated tests to conda packaging

--- tools/conda/pvapy-conda/build.sh
+++ tools/conda/pvapy-conda/build.sh
@@ -11,6 +11,7 @@ CONDA_BOOST_DIR=$PREFIX
 OPT_PVAPY_DIR=opt/pvapy
 CONDA_PVAPY_DIR=$PREFIX/$OPT_PVAPY_DIR
 CONDA_PVAPY_DOC_DIR=$CONDA_PVAPY_DIR/doc
+CONDA_PVAPY_EXAMPLES_DIR=$CONDA_PVAPY_DIR/examples
 
 PYTHON_MAJOR_MINOR_VERSION=`python --version 2>&1 | cut -f2 -d ' ' | cut -f1,2 -d '.'`
 PYTHON_MAJOR_VERSION=`echo $PYTHON_MAJOR_MINOR_VERSION | cut -f1 -d '.'`
@@ -31,6 +32,19 @@ make doc
 mkdir -p $CONDA_PVAPY_DOC_DIR
 rsync -arvlP documentation/sphinx/_build/html $CONDA_PVAPY_DOC_DIR/
 
+echo "Generating pvapy modules"
+SITE_PACKAGES_DIR=$PREFIX/lib/python$PYTHON_MAJOR_MINOR_VERSION/site-packages
+PVACCESS_DIR=$SITE_PACKAGES_DIR/pvaccess
+PVAPY_DIR=$SITE_PACKAGES_DIR/pvapy
+mkdir -p $PVACCESS_DIR
+mkdir -p $PVAPY_DIR
+
+echo "from .pvaccess import *" > $PVACCESS_DIR/__init__.py
+echo "from pvaccess import *" > $PVAPY_DIR/__init__.py
+
 echo "Installing pvapy library"
-rsync -arv $PVACCESS_LIB_DIR/pvaccess.so $PREFIX/lib/python$PYTHON_MAJOR_MINOR_VERSION/lib-dynload
+#rsync -arv $PVACCESS_LIB_DIR/pvaccess.so $PREFIX/lib/python$PYTHON_MAJOR_MINOR_VERSION/lib-dynload
+rsync -arv $PVACCESS_LIB_DIR/pvaccess.so $PVACCESS_DIR/
+
+
 
