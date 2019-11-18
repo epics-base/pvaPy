adcc25cafc5ae9fc2a71d8faccd202505bcba683
Sinisa Veseli <sveseli@aps.anl.gov>
2019-11-18 16:42:44 -0600

    conda yaml file updated

--- tools/conda/pvapy-conda/run_test.sh
+++ tools/conda/pvapy-conda/run_test.sh
@@ -2,12 +2,13 @@
 
 echo "Starting test server"
 cd test
-python testServer.py 10  &
+python testServer.py 15  &
 
-echo "Starting tests"
-nosetests -v testPvObject.py
-nosetests -v testChannelPut.py
-nosetests -v testMultiChannel.py
+for f in PvObject ChannelPut MultiChannel; do
+    echo "Starting $f tests"
+    sleep 1
+    nosetests -v test$f.py
+done
 
 wait 
 echo "Tests done"
