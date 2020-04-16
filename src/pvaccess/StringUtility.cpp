66b49b21884cced79c7c88b56495472e37ae3994
Sinisa Veseli <sveseli@aps.anl.gov>
2020-04-16 16:10:04 -0500

    add return statements

--- src/pvaccess/StringUtility.cpp
+++ src/pvaccess/StringUtility.cpp
@@ -50,12 +50,14 @@ std::string& leftTrim(std::string& s)
     s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
         return !std::isspace(ch);
     }));
+    return s;
 }
 
 std::string& rightTrim(std::string& s) 
     s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
         return !std::isspace(ch);
     }).base(), s.end());
+    return s;
 }
 #endif
 
