15982bc1168833e34b48b22ad38bc3dd6195d25f
Sinisa Veseli <sveseli@aps.anl.gov>
2020-06-12 13:04:27 -0500

    fix for python object to string conversion

--- src/pvaccess/PyUtility.cpp
+++ src/pvaccess/PyUtility.cpp
@@ -10,68 +10,13 @@ namespace PyUtility
 
 std::string extractStringFromPyObject(const boost::python::object& pyObject)
 {
-    // Simply try to extract various data types until one works.
-    // There has to be a better way for doing this.
-    boost::python::extract<std::string> extractStringValue(pyObject);
-    if (extractStringValue.check()) {
-        return extractStringValue();
+    try {
+        return boost::python::extract<std::string>(boost::python::str(pyObject))();
     }
-
-    boost::python::extract<double> extractDoubleValue(pyObject);
-    if (extractDoubleValue.check()) {
-        return StringUtility::toString<double>(extractDoubleValue());
+    catch(const boost::python::error_already_set&) {
+        PyErr_Print();
+        PyErr_Clear();
     }
-
-    boost::python::extract<float> extractFloatValue(pyObject);
-    if (extractFloatValue.check()) {
-        return StringUtility::toString<float>(extractFloatValue());
-    }
-
-    boost::python::extract<unsigned long long> extractULongLongValue(pyObject);
-    if (extractULongLongValue.check()) {
-        return StringUtility::toString<unsigned long long>(extractULongLongValue());
-    }
-
-    boost::python::extract<long long> extractLongLongValue(pyObject);
-    if (extractLongLongValue.check()) {
-        return StringUtility::toString<long long>(extractLongLongValue());
-    }
-
-    boost::python::extract<unsigned int> extractUIntValue(pyObject);
-    if (extractUIntValue.check()) {
-        return StringUtility::toString<unsigned int>(extractUIntValue());
-    }
-
-    boost::python::extract<int> extractIntValue(pyObject);
-    if (extractIntValue.check()) {
-        return StringUtility::toString<int>(extractIntValue());
-    }
-
-    boost::python::extract<unsigned short> extractUShortValue(pyObject);
-    if (extractUShortValue.check()) {
-        return StringUtility::toString<unsigned short>(extractUShortValue());
-    }
-
-    boost::python::extract<short> extractShortValue(pyObject);
-    if (extractShortValue.check()) {
-        return StringUtility::toString<short>(extractShortValue());
-    }
-
-    boost::python::extract<unsigned char> extractUCharValue(pyObject);
-    if (extractUCharValue.check()) {
-        return StringUtility::toString<unsigned char>(extractUCharValue());
-    }
-
-    boost::python::extract<char> extractCharValue(pyObject);
-    if (extractCharValue.check()) {
-        return StringUtility::toString<char>(extractCharValue());
-    }
-
-    boost::python::extract<bool> extractBoolValue(pyObject);
-    if (extractBoolValue.check()) {
-        return StringUtility::toString<bool>(extractBoolValue());
-    }
-
     throw InvalidArgument("Unable to convert object to string.");
 }
 
