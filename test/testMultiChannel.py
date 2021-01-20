d5a77514235b9b5b2f5c16847f3898e42459f216
Sinisa Veseli <sveseli@aps.anl.gov>
2021-01-19 19:45:09 -0600

    add test for multichannel put

--- test/testMultiChannel.py
+++ test/testMultiChannel.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 from pvaccess import MultiChannel
+from pvaccess import PvInt
+from pvaccess import PvDouble
 from testUtility import TestUtility
 
 class TestMultiChannel:
@@ -19,6 +21,25 @@ class TestMultiChannel:
         dc.put(dv)
 
         mc = MultiChannel([ic.getName(),dc.getName()])
+        pv = mc.get()
+        iv2 = pv['value'][0][0]['value']
+        assert(iv2 == iv)
+        dv2 = pv['value'][1][0]['value']
+        TestUtility.assertDoubleEquality(dv,dv2)
+
+    #
+    # MultiChannel Put
+    #
+
+    def testPut_IntDouble(self):
+        iv = TestUtility.getRandomInt()
+        ic = TestUtility.getIntChannel()
+
+        dv = TestUtility.getRandomDouble()
+        dc = TestUtility.getDoubleChannel()
+
+        mc = MultiChannel([ic.getName(),dc.getName()])
+        mc.put([PvInt(iv),PvDouble(dv)])
         pv = mc.get()
         iv2 = pv['value'][0][0]['value']
         assert(iv2 == iv)
