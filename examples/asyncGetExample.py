cdc7035717efa858cf2bbcd29a77eb1b1eb7e188
Sinisa Veseli <sveseli@aps.anl.gov>
2021-08-12 22:13:46 -0500

    add asyncGet example

---
+++ examples/asyncGetExample.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import time
+from threading import Thread
+from pvaccess import *
+
+class MyThread(Thread):
+    def __init__(self, n):
+        Thread.__init__(self)
+        self.n = n
+
+    def run(self):
+        while self.n > 0:
+            print('Time is %.2f' % time.time())
+            time.sleep(0.5)
+            self.n-=1
+
+def echo(pv):
+    print('Got PV: %s' % pv)
+
+if __name__ == '__main__':
+    c = Channel('testInt')
+    t = MyThread(10)
+    t.start()
+    time.sleep(1)
+    c.asyncGet(echo)
+    print('Started Async Get')
+    time.sleep(5)
+    
