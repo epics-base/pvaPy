6e12295a88d0a3fe8f3cab180505190cf7bee37e
Sinisa Veseli <sveseli@anl.gov>
2022-07-07 22:15:18 -0500

    add non-pva specific constants

---
+++ src/pvaccess/PvaPyConstants.cpp
@@ -0,0 +1,9 @@
+// Copyright information and license terms for this software can be
+// found in the file LICENSE that is included with the distribution
+
+#include "PvaPyConstants.h"
+
+const char* PvaPyConstants::NumReceivedCounterKey("nReceived");
+const char* PvaPyConstants::NumRejectedCounterKey("nRejected");
+const char* PvaPyConstants::NumDeliveredCounterKey("nDelivered");
+const char* PvaPyConstants::NumOverrunsCounterKey("nOverruns");
---
+++ src/pvaccess/PvaPyConstants.h
@@ -0,0 +1,19 @@
+// Copyright information and license terms for this software can be
+// found in the file LICENSE that is included with the distribution
+
+#ifndef PVAPY_CONSTANTS_H
+#define PVAPY_CONSTANTS_H
+
+#include <string>
+
+class PvaPyConstants
+{
+public:
+    static const char* NumReceivedCounterKey;
+    static const char* NumRejectedCounterKey;
+    static const char* NumDeliveredCounterKey;
+    static const char* NumOverrunsCounterKey;
+}; 
+
+#endif
+
