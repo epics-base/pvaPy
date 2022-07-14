04b27793962aeca6c6c898ed5ea99313040508f6
Sinisa Veseli <sveseli@anl.gov>
2022-07-13 22:03:29 -0500

    add example for the data processor implementation

---
+++ examples/hpcDataProcessorExample.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from pvapy.hpc.dataProcessor import DataProcessor
+
+# Example for a data processor implementation
+class HpcDataProcessor(DataProcessor):
+
+    def __init__(self, configDict={}):
+        DataProcessor.__init__(self, configDict)
+
+    def start(self):
+        DataProcessor.start(self)
+
+    def stop(self):
+        DataProcessor.stop(self)
+
+    def getStats(self):
+        return DataProcessor.getStats(self)
+
+    def updateStats(self, t=0):
+        return DataProcessor.updateStats(self, t)
+
+    def process(self, pvObject):
+        return DataProcessor.process(self, pvObject)
+
