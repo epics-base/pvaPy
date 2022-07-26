aad0ebc446c0a93e74c44514fc76cc29add9d4a3
Sinisa Veseli <sveseli@anl.gov>
2022-07-26 10:13:20 -0500

    add comments to example

--- examples/hpcDataProcessorExample.py
+++ examples/hpcDataProcessorExample.py
@@ -2,30 +2,37 @@
 
 from pvapy.hpc.userDataProcessor import UserDataProcessor
 
-# Example for a trivial data processor implementation
+# Trivial data processor implementation
+# All interface hooks are optional except for process()
 class HpcDataProcessor(UserDataProcessor):
 
     def __init__(self, configDict={}):
         UserDataProcessor.__init__(self, configDict)
 
+    # Method called at start
     def start(self):
         pass
 
+    # Configure user processor
     def configure(self, kwargs):
         pass
 
+    # Process monitor update
     def process(self, pvObject):
         return pvObject
 
+    # Method called at shutdown
     def stop(self):
         pass
 
+    # Reset statistics for user processor
     def resetStats(self):
         pass
 
+    # Retrieve statistics for user processor
     def getStats(self):
         return {}
 
+    # Define PVA types for different stats variables
     def getStatsPvaTypes(self):
         return {}
-
