291b9f26f40df2ab70eb3ec2a63f08436b763b09
Sinisa Veseli <sveseli@anl.gov>
2022-07-26 12:52:10 -0500

    add ability to import data processor from classpath

--- pvapy/utility/objectUtility.py
+++ pvapy/utility/objectUtility.py
@@ -3,16 +3,31 @@
 class ObjectUtility:
 
     @classmethod
-    def createObjectInstanceFromFile(cls, filePath, moduleName, className, args=None):
-        import importlib.util
-        spec = importlib.util.spec_from_file_location(moduleName, filePath)
-        m = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(m)
-        c = m.__dict__.get(className)
+    def createObjectInstance(cls, module, className, args=None):
+        c = module.__dict__.get(className)
         o = None
         if c:
             if args:
                 o = c(args)
             else:
                 o = c()
+        if not o:
+            raise pva.InvalidArgument(f'Could not create object instance of class {className} from module {module}')
         return o 
+
+    @classmethod
+    def createObjectInstanceFromFile(cls, filePath, moduleName, className, args=None):
+        import importlib.util
+        spec = importlib.util.spec_from_file_location(moduleName, filePath)
+        m = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(m)
+        return cls.createObjectInstance(m, className, args)
+
+    @classmethod
+    def createObjectInstanceFromClassPath(cls, classPath, args=None):
+        import importlib
+        moduleName = '.'.join(classPath.split('.')[:-1])
+        className = classPath.split('.')[-1]
+        m = importlib.import_module(moduleName)
+        return cls.createObjectInstance(m, className, args)
+
