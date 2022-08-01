79f702c7ca65597e61af53519aa7e71ac4c05d16
Sinisa Veseli <sveseli@anl.gov>
2022-08-01 08:21:24 -0500

    add utility method to replace image rather than generating it

--- pvapy/hpc/adImageProcessor.py
+++ pvapy/hpc/adImageProcessor.py
@@ -15,12 +15,18 @@ class AdImageProcessor(UserDataProcessor):
         return AdImageUtility.reshapeNtNdArray(ntnda)
 
     @classmethod
-    def getNtNdArrayDataTypeKey(cls, image):
-        return AdImageUtility.getNtNdArrayDataTypeKey(image)
+    def getNtNdArrayDataFieldKey(cls, image):
+        return AdImageUtility.getNtNdArrayDataFieldKey(image)
 
+    # Generates new NtNdArray object
     @classmethod
     def generateNtNdArray2D(cls, imageId, image, extraFieldsPvObject=None):
         return AdImageUtility.generateNtNdArray2D(imageId, image, extraFieldsPvObject)
+
+    # Replaces image data in the existing NtNdArray object
+    @classmethod
+    def replaceNtNdArrayImage2D(cls, ntnda, imageId, image, extraFieldsPvObject=None):
+        return AdImageUtility.replaceNtNdArrayImage2D(ntnda, imageId, image, extraFieldsPvObject)
 
     # Process monitor update
     def process(self, pvObject):
