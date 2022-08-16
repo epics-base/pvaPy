616aaa7aa877db2b646fdf079c0376405c61bcc6
Sinisa Veseli <sveseli@anl.gov>
2022-08-16 16:42:05 -0500

    more work on iocsh interfaces

--- src/pvaccess/CaIoc.cpp
+++ src/pvaccess/CaIoc.cpp
@@ -4,6 +4,10 @@
 
 #include <dbAccess.h>
 #include <dbTest.h>
+#include "InvalidState.h"
+#include "InvalidArgument.h"
+#include "InvalidRequest.h"
+#include "ObjectNotFound.h"
 #include "CaIoc.h"
 
 extern "C" int pvapy_registerRecordDeviceDriver(struct dbBase *pbase);
@@ -43,18 +47,141 @@ void CaIoc::loadRecords(const std::string& fileName, const std::string& substitu
     dbLoadRecords(fileName.c_str(), substitutions.c_str());
 }
 
-void CaIoc::listRecords(const std::string& recordType, const std::string& fields)
+// Modified from the dbl() code in dbTest.h to return list of record names
+bp::list getRecordNames()
 {
-    dbl(recordType.c_str(), fields.c_str());
+    bp::list recordNames;
+
+    DBENTRY dbentry;
+    DBENTRY *pdbentry=&dbentry;
+    int status;
+
+    if (!pdbbase) {
+        throw InvalidState("No database loaded.");
+    }
+    dbInitEntry(pdbbase, pdbentry);
+    while (!status) {
+        status = dbFirstRecord(pdbentry);
+        while (!status) {
+            recordNames.append(dbGetRecordName(pdbentry)));
+            status = dbNextRecord(pdbentry);
+        }
+    }
+    dbFinishEntry(pdbentry);
+    return recordNames;
 }
 
 void CaIoc::putField(const std::string& name, const std::string& value)
 {
-    dbpf(name.c_str(), value.c_str());
+    DBADDR addr;
+    long status;
+    short dbrType = DBR_STRING;
+    long n = 1;
+    char *array = NULL;
+    const char* pname = name.c_str();
+    const char* pvalue = value.c_str();
+
+    if (name.size() == 0) {
+        throw InvalidArgument("Record name cannot be empty.");
+    }
+
+    if (nameToAddr(pname, &addr)) {
+        throw ObjectNotFound("Record name not found.");
+    }
+
+    if (addr.precord->lset == NULL) {
+        throw InvalidState("IOC must be started before putField() can be called.");
+    }
+
+    if (addr.no_elements > 1) {
+        dbrType = addr.dbr_field_type;
+        if (addr.dbr_field_type == DBR_CHAR || addr.dbr_field_type == DBR_UCHAR) {
+            n = (long)strlen(pvalue) + 1;
+        } else {
+            n = addr.no_elements;
+            array = calloc(n, dbValueSize(dbrType));
+            if (!array) {
+                InvalidState("Out of memory.");
+            }
+            status = dbPutConvertJSON(pvalue, dbrType, array, &n);
+            if (status) {
+                throw InvalidRequest("dbPutConvertJSON returned status: " + StringUtility::toString<int>(status));
+            }
+            pvalue = array;
+        }
+    }
+    status = dbPutField(&addr, dbrType, pvalue, n);
+    if (status) {
+        throw InvalidRequest("dbPutField returned status: " + StringUtility::toString<int>(status));
+    }
+    pvalue = array;
+    free(array);
 }
 
 void CaIoc::printRecord(const std::string& name, int level)
 {
-    dbpr(name.c_str(), level);
+    static TAB_BUFFER msg_Buff;
+    TAB_BUFFER *pMsgBuff = &msg_Buff;
+    DBADDR addr;
+    char *pmsg;
+    int tab_size = 20;
+    const char* pname = name.c_str();
+
+    if (name.size() == 0) {
+        throw InvalidArgument("Record name cannot be empty.");
+    }
+
+    if (nameToAddr(pname, &addr)) {
+        throw ObjectNotFound("Record name not found.");
+    }
+
+    pmsg = pMsgBuff->message;
+
+    int status = dbpr_report(pname, &addr, level, pMsgBuff, tab_size);
+    if (status) {
+        throw InvalidRequest("dbpr_report returned status: " + StringUtility::toString<int>(status));
+    }
+
+    pmsg[0] = '\0';
+    dbpr_msgOut(pMsgBuff, tab_size);
+    return 0;
+}
 }
 
+//
+// Wrappers for calls available in iocsh
+//
+int CaIoc::dbLoadDatabase(const std::string& fileName, const std::string& path, const std::string& substitutions)
+{
+    return dbLoadDatabase(fileName.c_str(), path.c_str(), substitutions.c_str());
+}
+
+int CaIoc::dbLoadRecords(const std::string& fileName, const std::string& substitutions)
+{
+    return dbLoadRecords(fileName.c_str(), substitutions.c_str());
+}
+
+int CaIoc::iocInit() 
+{
+    return iocInit();
+}
+
+int CaIoc::dbl(const std::string& recordType, const std::string& fields)
+{
+    return dbl(recordType.c_str(), fields.c_str());
+}
+
+int CaIoc::dbpr(const std::string& name, int level)
+{
+   return dbpr(name.c_str(), level);
+}
+
+int CaIoc::dbgf(const std::string& name)
+{
+    return dbgf(name.c_str());
+}
+
+int CaIoc::dbpf(const std::string& name, const std::string& value)
+{
+    return dbpf(name.c_str(), value.c_str());
+}
--- src/pvaccess/CaIoc.h
+++ src/pvaccess/CaIoc.h
@@ -4,6 +4,7 @@
 #ifndef CA_IOC_H
 #define CA_IOC_H
 
+#include <boost/python/list.hpp>
 #include <iocInit.h>
 #include "PvaPyLogger.h"
 
@@ -19,9 +20,18 @@ public:
     virtual void loadDatabase(const std::string& fileName, const std::string& path, const std::string& substitutions); 
     virtual void registerDeviceDriver();
     virtual void loadRecords(const std::string& fileName, const std::string& substitutions);
-    virtual void listRecords(const std::string& recordType, const std::string& fields);
+    virtual boost::python::list getRecordNames();
     virtual void putField(const std::string& name, const std::string& value);
     virtual void printRecord(const std::string& name, int level);
+
+    // Wrappers for calls available in iocsh
+    virtual int dbLoadDatabase(const std::string& fileName, const std::string& path, const std::string& substitutions); 
+    virtual int dbLoadRecords(const std::string& fileName, const std::string& substitutions);
+    virtual int iocInit();
+    virtual int dbl(const std::string& recordType, const std::string& fields);
+    virtual int dbpr(const std::string& name, int level);
+    virtual int dbgf(const std::string& name);
+    virtual int dbpf(const std::string& name, const std::string& value);
 
 private:
     static PvaPyLogger logger;
