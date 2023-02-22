f7045caac4b77f9f308d9b722665189bc7378d81
Sinisa Veseli <sveseli@anl.gov>
2023-02-22 17:02:22 -0600

    switch from nose to pytest for running tests

--- tools/conda/pvapy-conda/run_test.sh
+++ tools/conda/pvapy-conda/run_test.sh
@@ -5,7 +5,8 @@ cd test
 python testServer.py 30 &
 
 echo "Starting tests"
-nosetests -sv
+TEST_FILES=`ls -c1 test*.py | grep -v testUtility | grep -v testServer.py`
+pytest -sx $TEST_FILES
 
 wait
 echo "Tests done"
