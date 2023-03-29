75e66438bb788945210ef5fca634eb34130ab828
Sinisa Veseli <sveseli@anl.gov>
2023-03-29 15:56:53 -0500

    add tests for all CLIs

--- test/testAdSimServerCli.py
+++ test/testAdSimServerCli.py
@@ -1,15 +1,15 @@
 '''
-Test HPC Consumer CLI.
+Test AD Sim Server CLI.
 '''
 from unittest.mock import Mock
 import sys
 import pylint.lint
 
-from pvapy.cli import hpcConsumer
+from pvapy.cli import adSimServer
 
 def testLint(monkeypatch):
     ''' Test for linting errors '''
     monkeypatch.setattr(sys, 'exit', Mock())
-    pylint_opts = ['pvapy.cli.hpcConsumer', '--disable=all', '--enable=E,F', '--generated-members="pva.*"']
+    pylint_opts = ['pvapy.cli.adSimServer', '--disable=all', '--enable=E,F', '--generated-members="pva.*"']
     pylint.lint.Run(pylint_opts)
     sys.exit.assert_called_once_with(0)
