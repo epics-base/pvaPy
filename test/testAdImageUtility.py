33f4352e0386a88f62ecf5edb68e3df0f7b3013e
Sinisa Veseli <sveseli@anl.gov>
2024-05-31 15:41:59 -0500

    disable E1101 due to false result for ndarray.__array_interface__ method

--- test/testAdImageUtility.py
+++ test/testAdImageUtility.py
@@ -11,6 +11,7 @@ from pvapy.utility.adImageUtility import AdImageUtility
 def testLint(monkeypatch):
     ''' Test for linting errors '''
     monkeypatch.setattr(sys, 'exit', Mock())
-    pylint_opts = ['pvapy.utility.adImageUtility', '--disable=all', '--enable=E,F', '--generated-members="pva.*"']
+    # Disable E1101 due to a false result for numpy.ndarray.__array__interface__ method
+    pylint_opts = ['pvapy.utility.adImageUtility', '--disable=all', '--enable=E,F', '--disable=E1101', '--generated-members="pva.*"']
     pylint.lint.Run(pylint_opts)
     sys.exit.assert_called_once_with(0)
