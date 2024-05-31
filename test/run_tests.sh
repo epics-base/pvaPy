05e4d3ec69d15fa5539bcfc097afc31ecb04f587
Sinisa Veseli <sveseli@anl.gov>
2024-05-31 15:46:10 -0500

    add ability to run specific tests, pvaccess only, or all tests

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,26 +1,31 @@
 #!/bin/sh
 
-echo "Starting test server"
-python testServer.py 30 &
+TEST_SERVER_RUNTIME=30
+TEST_FILES_ALL=`ls -c1 test*.py | grep -v testUtility | grep -v testServer.py`
+TEST_FILES_PVACCESS=`grep pvaccess test*.py | cut -f1 -d':' | sort -u | grep -v testUtility | grep -v testServer.py`
 
-echo "Starting tests"
-TEST_FILES=`ls -c1 test*.py | grep -v testUtility | grep -v testServer.py`
-EXIT_STATUS=0
-FAILED_TEST=""
-for T in $TEST_FILES; do
-    echo "Running test: $T"
-    pytest -sx $T
-    if [ $? -ne 0 ]; then
-        EXIT_STATUS=1
-        FAILED_TESTS="$FAILED_TESTS $T"
-    fi
-done
+TEST_FILES=$@
+RUN_SPECIFIC_TESTS=`echo $TEST_FILES | grep test`
+if [ -z "$TEST_FILES" ]; then
+    TEST_FILES=$TEST_FILES_PVACCESS
+elif [ -z "$RUN_SPECIFIC_TESTS" ]; then
+    TEST_FILES=$TEST_FILES_ALL
+    TEST_SERVER_RUNTIME=60
+fi
+
+echo "Starting test server with runtime $TEST_SERVER_RUNTIME"
+python testServer.py $TEST_SERVER_RUNTIME &
+
+echo "Running tests:"
+echo $TEST_FILES
+echo
+
+pytest -s $TEST_FILES
+EXIT_STATUS=$?
 
 wait
 echo "Tests done, exit status: $EXIT_STATUS"
 if [ $EXIT_STATUS -eq 0 ]; then
     echo "All tests passed"
-else
-    echo "Failed tests: $FAILED_TESTS"
 fi
 exit $EXIT_STATUS
