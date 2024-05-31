a8d69483c7cd0ab5752db2211b67712d7ff492b2
Sinisa Veseli <sveseli@anl.gov>
2024-05-31 15:55:25 -0500

    update pip build files

--- tools/pip/pvapy-pip/run_test.sh
+++ tools/pip/pvapy-pip/run_test.sh
@@ -2,29 +2,4 @@
 
 TEST_DIR=../../../test
 cd $TEST_DIR
-
-echo "Starting test server"
-python testServer.py 30 &
-
-echo "Starting tests"
-TEST_FILES=`ls -c1 test*.py | grep -v testUtility | grep -v testServer.py`
-EXIT_STATUS=0
-FAILED_TEST=""
-for T in $TEST_FILES; do
-    echo "Running test: $T"
-    pytest -sx $T
-    if [ $? -ne 0 ]; then
-        EXIT_STATUS=1
-        FAILED_TESTS="$FAILED_TESTS $T"
-    fi
-done
-
-wait
-echo "Tests done, exit status: $EXIT_STATUS"
-if [ $EXIT_STATUS -eq 0 ]; then
-    echo "All tests passed"
-else
-    echo "Failed tests: $FAILED_TESTS"
-fi
-exit $EXIT_STATUS
-
+./run_tests.sh $@
