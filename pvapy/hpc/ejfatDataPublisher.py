5df7f55b5206e2a1c51c2a521323cb5d683943de
Sinisa Veseli <sveseli@anl.gov>
2025-07-03 17:52:05 -0500

    prepare for callback usage; e2sar python binding still has issue with gil management, so use sendEvent() for now

--- pvapy/hpc/ejfatDataPublisher.py
+++ pvapy/hpc/ejfatDataPublisher.py
@@ -24,14 +24,26 @@ from ..utility.loggingManager import LoggingManager
 class EjfatDataPublisher(DataPublisher, EjfatSystemBase):
     ''' EJFAT data publisher class. '''
 
+    BUFFER_CACHE_SIZE_KEY = 'bufferCacheSize'
+
     DEFAULT_DATA_ID = 0
     DEFAULT_EVENT_SOURCE_ID = 1
+    DEFAULT_BUFFER_CACHE_SIZE = 100
 
     # Valid Segmenter attributes: 'connectedSocket', 'dpV6', 'getFromINI', 'mtu', 'numSendSockets', 'sndSocketBufSize', 'syncPeriodMs', 'syncPeriods', 'useCP', 'usecAsEventNum', 'zeroRate'
 
     def __init__(self, outputChannel, configDict={}):
         DataPublisher.__init__(self, outputChannel)
         EjfatSystemBase.__init__(self, outputChannel, configDict)
+        self.bufferMap = {}
+        self.bufferIndex = 0
+        self.bufferMapLock = threading.Lock()
+
+        self.bufferCacheSize = int(configDict.get(self.BUFFER_CACHE_SIZE_KEY, self.DEFAULT_BUFFER_CACHE_SIZE))
+        if self.bufferCacheSize < 1:
+            self.bufferCacheSize = self.DEFAULT_BUFFER_CACHE_SIZE
+        self.logger.debug('Using send buffer cache size: %s', self.bufferCacheSize)
+        self.bufferIndexSet = set(list(range(0, self.bufferCacheSize)))
 
         self.logger.debug('Initializing segmenter flags')
         sflags = e2sar_py.DataPlane.Segmenter.SegmenterFlags()
@@ -60,13 +72,40 @@ class EjfatDataPublisher(DataPublisher, EjfatSystemBase):
         except Exception as ex:
             raise pva.ConfigurationError(f'Cannot start EJFAT data publisher: {ex}')
 
+    def __prepareSendBuffer(self, pvObject):
+        with self.bufferMapLock:
+            usedBufferIndexSet = set(list(self.bufferMap.keys()))
+            availableBuffers = sorted(self.bufferIndexSet-usedBufferIndexSet)
+            if not availableBuffers:
+                raise Exception('No free buffers available')
+            bufferIndex = availableBuffers[0]
+            self.bufferMap[bufferIndex] = bufferIndex
+        self.bufferMap[bufferIndex] = pickle.dumps(pvObject)
+        bufferLength = len(self.bufferMap[bufferIndex])
+        return bufferIndex, bufferLength
+
+    def __sendBufferCallback(self, bufferIndex):
+        with self.bufferMapLock:
+            self.logger.debug('Freeing buffer index %s', bufferIndex)
+            del self.bufferMap[bufferIndex]
+            self.nPublished += 1
+
     def updateOutputChannel(self, pvObject):
         try:
-            buffer = pickle.dumps(pvObject)
-            bLen = len(buffer)
-            self.logger.debug('Buffering object of size %s', bLen)
-            self.segmenter.addToSendQueue(buffer, bLen)
-            self.nPublished += 1
+            bufferIndex, bufferLength = self.__prepareSendBuffer(pvObject)
+            self.logger.debug('Using buffer index %s to add to queue object of size %s', bufferIndex, bufferLength)
+            #self.segmenter.addToSendQueue(self.bufferMap[bufferIndex], bufferLength, callback=self.__sendBufferCallback, cbArg=bufferIndex)
+            self.segmenter.sendEvent(self.bufferMap[bufferIndex], bufferLength)
+            stats = self.segmenter.getSendStats()
+            if stats.lastErrno != 0:
+                self.nErrors += 1
+                self.logger.warning('Last segmenter error: %s', stats.lastErrno)
+            else:
+                self.nPublished += 1
+            del self.bufferMap[bufferIndex]
         except Exception as ex:
             self.nErrors += 1
             self.logger.error('Error publishing object: %s', ex)
+            stats = self.segmenter.getSendStats()
+            if stats.lastErrno != 0:
+                self.logger.warning('Last segmenter error: %s', stats.lastErrno)
